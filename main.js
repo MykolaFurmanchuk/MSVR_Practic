af77e7ba1b22b132832bd9a0bbb8ad411b581921
Mykola <mikolafurman2001@gmail.com>
2023-06-06 20:52:46 +0300

    add ReadMagnetometer function

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ let texture;
 let CanvasWidth;
 let CanvasHeight;
 let BackgroundVideoModel;
+let RotationMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
@@ -126,7 +127,8 @@ function draw() {
 }
 
 function DrawSurface() {
-    let modelView = spaceball.getViewMatrix();
+
+    let modelView = spaceball.RotationMatrix();
     let translateToPointZero = m4.translation(0, 0, 0);
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, m4.multiply(stereoCamera.mModelViewMatrix, m4.multiply(m4.multiply(stereoCamera.mProjectionMatrix, translateToPointZero), modelView)));
     gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -295,6 +297,58 @@ function getPointLocation(){
     pointList.push(x,y,z);
     return pointList;
 }
+
+function ReadMagnetometer() {
+
+    timeStamp = Date.now();
+    let sensor = new Magnetometer({
+        frequency: 60
+    });
+    sensor.addEventListener('reading', e => {
+        x = sensor.x
+        y = sensor.y
+        z = sensor.z
+        RotationMatrix = getRotationMatrix(x, y, z)
+        
+    });
+    sensor.start();
+
+}
+
+let degtorad = Math.PI / 180;
+
+function getRotationMatrix(alpha, beta, gamma) {
+
+    var _x = beta ? beta * degtorad : 0;
+    var _y = gamma ? gamma * degtorad : 0;
+    var _z = alpha ? alpha * degtorad : 0;
+    
+    var cX = Math.cos(_x);
+    var cY = Math.cos(_y);
+    var cZ = Math.cos(_z);
+    var sX = Math.sin(_x);
+    var sY = Math.sin(_y);
+    var sZ = Math.sin(_z);
+
+    var m11 = cZ * cY - sZ * sX * sY;
+    var m12 = -cX * sZ;
+    var m13 = cY * sZ * sX + cZ * sY;
+
+    var m21 = cY * sZ + cZ * sX * sY;
+    var m22 = cZ * cX;
+    var m23 = sZ * sY - cZ * cY * sX;
+
+    var m31 = -cX * sY;
+    var m32 = sX;
+    var m33 = cX * cY;
+
+    return [
+        m11, m12, m13,
+        m21, m22, m23,
+        m31, m32, m33
+    ];
+
+};
 
 function StereoCamera(
     Convergence,
@@ -467,6 +521,7 @@ function createProgram(gl, vShader, fShader) {
  * initialization function that will be called when the page has loaded
  */
 function init() {
+    ReadMagnetometer()
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
